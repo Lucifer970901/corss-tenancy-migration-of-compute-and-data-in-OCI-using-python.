534d89be5e9171e5e6853d843495ebf4fc64d708
Meghashree N <meghashreetendulkar@gmail.com>
2024-06-18 09:47:01 +0530

    Update 3. Import_image_and_backup.py

--- 3. Import_image_and_backup.py
+++ 3. Import_image_and_backup.py
@@ -21,7 +21,7 @@ for ad in availability_domains.data:
      break
 
 print(availability_domain)
-
+#create the volume from backup.
 with open('Volume_Backup_details.csv', 'r') as file:
     csv_reader = csv.reader(file)
     next(file) #skip the first line/header from listing 
@@ -36,3 +36,22 @@ with open('Volume_Backup_details.csv', 'r') as file:
     )
 # Get the data from response
 print(create_volume_response.data)
+
+# Create an image source details object
+image_source_details = oci.core.models.ImageSourceDetails(
+    source_type="objectStorageUri",
+    source_uri = "https://sehubjapaciaas.objectstorage.us-ashburn-1.oci.customer-oci.com/p/HcxqBP_FXNcO1hA3AFQFxDLd2srx9dlL8BrEa8NfmfGF9R-pfwDBXMX0pqmIHXW7/n/sehubjapaciaas/b/test_bucket/o/exported-image-python-mysql",
+    os_type="linux",
+)
+
+# Create an import image request
+create_image_request = oci.core.models.CreateImageDetails(
+    compartment_id=compartment_id,
+    display_name=image_display_name,
+    launch_mode="NATIVE",
+    image_source_details=image_source_details,
+)
+
+# Initiate the import image job
+create_image_response = compute_client.create_image(create_image_request)
+print(create_image_response.data)
